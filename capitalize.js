02a0f78c1a5a0747a1edfdfe90be8d04ecc1782e
laxatzi <laxatzi@yahoo.com>
2020-03-06 12:23:50 +0200

    create a function to cap text the old way

--- capitalize.js
+++ capitalize.js
@@ -1,1 +1,20 @@
+// OLD CODE 
+var result;
+function getText(){
+   var someText = prompt("In which city do you leave?");
+   capWords(someText);
+   alert(result.join(" "));
+}
 
+function capWords(input){
+   var inputArray = input.split(" ");
+   var transformed = "";
+   result=[];
+   for(var i =0; i<inputArray.length; i++){
+      transformed = [
+         inputArray[i].charAt(0).toUpperCase(),
+         inputArray[i].substring(1)
+      ].join("");
+      result.push(transformed);
+   }
+}
