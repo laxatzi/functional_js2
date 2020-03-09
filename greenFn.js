29f4864e82e83f9bd2877e97510ce5bae875b0fd
laxatzi <laxatzi@yahoo.com>
2020-03-09 12:35:06 +0200

    create capitalize program with functional programming principles

--- greenFn.js
+++ greenFn.js
@@ -1,2 +1,28 @@
-// OLD CODE 
+// Capitalize with Functional Programming
+/* 
+  FUNCTIONAL PROGRAMMING STYLE CHARACTERISTICS 
 
+    --Take advantage of functions as first-class objects
+    --Don't change external values or state while executing functions
+    --Describe the problem or solution
+
+*/
+
+(function(){
+   'use strict';
+  
+   function capify(str){
+      return [str.charAt(0).toUpperCase(), str.substring(1)].join("");
+   }
+
+   function capifyMoreWords(fn, str){
+      return str.split(" ").map(fn).join(" ");
+   }
+
+   function getValue(e){
+      var feedback = prompt("Give me feedback! Which style of programming do you prefer?");
+      alert(capifyMoreWords(capify, feedback));
+   }
+
+   document.getElementById("button").addEventListener("click", getValue);
+}());//end wrapper iife
