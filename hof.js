10a934db276fda6d377d467650225bae5a0b97ea
laxatzi <laxatzi@yahoo.com>
2020-03-10 13:58:23 +0200

    example of a higher order function

--- hof.js
+++ hof.js
@@ -1,1 +1,23 @@
+// Higher Order Functions
 
+/* 
+  Javascript treats functions as "first class citizens"
+    --The type of a function is Object 
+    --They can be assigned to a variable as a value
+    --They can be passed to another function as an argument
+    --They can be returned by another function as a result
+
+  A Higher Order Function is a function that takes another function as an argument
+  For example:
+  Both setInterval and setTimeout functions take functions as one of their arguments
+  See below:
+*/
+
+(function(){
+  'use strict';
+  const timer = setInterval(
+    ()=> console.log(new Date().toLocaleTimeString()),
+     1000
+  );
+  console.log(setTimeout(()=>clearInterval(timer), 3000));
+}());//end wrapper iife
