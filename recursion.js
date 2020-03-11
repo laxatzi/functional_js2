c34a314e7d7520a4520309bc19678268856fec87
laxatzi <laxatzi@yahoo.com>
2020-03-11 14:20:37 +0200

    refactor a factorial

--- recursion.js
+++ recursion.js
@@ -44,3 +44,50 @@
 }());//end wrapper iife
 
 // above code has the same number of local variables but it is at the same time cleaner.
+
+/* 
+  The most important element of a recursive function is a 'terminal condition'. 
+  Without it, the function will keep executing for ever.
+  Do factorial using recursion:
+
+*/
+(function(){
+  'use strict';
+    const factorial = (num)=>{
+       if(num <= 0){
+          return 1;
+       }
+       return (num * factorial(num - 1));
+    }
+
+    console.log(factorial(6)); // 720
+}());//end wrapper iife
+
+// Part of the value of using recursive functions is clean code.
+/* 
+  We are performing a stateless operation. Not changing the value of variables along the way.
+  Having no side effects outside of the functions. 
+  We are testing for the terminal condition first, allowing as to exit cleanly from our recursive function.
+*/
+
+// Proper Tail Calls
+/* 
+    In the above example we run the code 6 times (factorial(6)), and each time the code was being stacked into memory and maintained there while the first was running. 
+    Unfortunately this heavy memory usage can cause some issues. In some cases it is possible to get around this problem by using a technique called Proper Tail Calls.
+    With that technique your recursive function can rewrite values to the last frame of the memory stack. 
+    You do that by making sure the recursive call itself is in a tail position in the function.
+    In our recursive example, we can see that the final return of the function DOES return our factorial recursive, BUT it does it IN THE CONTEXT OF A CALCULATION. It is NOT being returned by itself. So the factorial doesn't implement a proper tail call.
+
+    Let's create an optimized function.
+*/
+
+(function(){
+  'use strict';
+    const factorialPTC = (number)=> factorIt(number, 1);
+    const factorIt = (number, accumulator)=> {
+       if(number<=0) { return accumulator; }
+       return factorIt(number -1, number * accumulator);
+       };
+ 
+      console.log(factorialPTC(6)); // 720
+}());//end wrapper iife
