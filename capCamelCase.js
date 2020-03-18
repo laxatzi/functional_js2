dbe9d618d3507faa7b08870b5bfc84a593d72539
laxatzi <laxatzi@yahoo.com>
2020-03-18 13:34:48 +0200

    resolve exercise by using a for loop

--- capCamelCase.js
+++ capCamelCase.js
@@ -1,1 +1,23 @@
+// Lets solve an exercise where we want to create a string of three letter words from an array of strings. (So we need to capitalize letters at the beginning of each word and then concatenate them in one single word)
 
+// lets try with the old way using a for loop
+
+(function(){
+  'use strict';
+
+  const animals = ['cat', 'dog', 'horse'];
+  let threeLetterAnimalsArr = [];
+  let threeLetterAnimals;
+  for(let j = 0; j < animals.length; j++){
+     let arrElem = animals[j];
+     if(arrElem.length === 3){
+        arrElem = arrElem.charAt(0).toUpperCase()+arrElem.slice(1);
+        threeLetterAnimalsArr.push(arrElem);
+     }
+  }
+
+  threeLetterAnimals = threeLetterAnimalsArr.join("");
+  
+  console.log(threeLetterAnimals); // CatDog
+
+}());//end wrapper iife
