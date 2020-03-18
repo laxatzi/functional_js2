e3d4a2e8b8eac9b28e713d075dc37761316a27bb
laxatzi <laxatzi@yahoo.com>
2020-03-18 10:50:45 +0200

    refactor filtering by replacing loop with filter mehtod

--- filter.js
+++ filter.js
@@ -22,3 +22,22 @@
   console.log(threeLetterAnimals); //['cat', 'dog'];
 
 }());//end wrapper iife
+
+
+// Refactor code replacing loop with filter method 
+
+(function(){
+  'use strict';
+
+  const animals = ['cat', 'dog', 'horse'];
+  let threeLetterAnimals = animals.filter((animal)=> animal.length === 3);
+
+  console.log(threeLetterAnimals); // ['cat', 'dog'];
+
+}());//end wrapper iife
+
+// comment on the above piece of code
+/* 
+  The filter method went through the array, applied the boolean analysis to each of the elements, and if the result was true, it added that element to the array that it was going to return. 
+  As a result, we got back an array that was filtered containing a subset of the elements that matched true for that boolean condition.
+*/
