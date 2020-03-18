f5d1b22dcf5264714a181924b1c0ec3e48a6e13e
laxatzi <laxatzi@yahoo.com>
2020-03-18 11:18:39 +0200

    clean code in filter.js file

--- filter.js
+++ filter.js
@@ -41,3 +41,26 @@
   The filter method went through the array, applied the boolean analysis to each of the elements, and if the result was true, it added that element to the array that it was going to return. 
   As a result, we got back an array that was filtered containing a subset of the elements that matched true for that boolean condition.
 */
+
+// lets make the code more clean by adding abstraction
+
+(function(){
+  'use strict';
+
+  const animals = ['cat', 'dog', 'horse'];
+  const checkLetters = (elem)=> elem.length === 3;
+  let threeLetterAnimals = animals.filter(checkLetters);
+
+  console.log(threeLetterAnimals); // ['cat', 'dog'];
+
+}());//end wrapper iife
+
+// We want to pass pure functions into our filter method. 
+/* 
+  Pure functions take a single value of the type that is in the array that we want to filter.
+  And they apply a test on this value without any side effects and without relying on the state of the application.
+  They would ALWAYS produce the same result for the same input value, and the functions that we want to pass into the filter method return always a boolean value. 
+  This should not alter any values outside of their scope, and of course, as with pure functions, they shouldn't alter any variables outside their scope.
+
+  Also, since they are pure they could potentially be used in other contexts without any modification.
+*/
