0d4f98bbfab6ebb2b9150aba5a15926491c2ec60
laxatzi <laxatzi@yahoo.com>
2020-03-20 17:10:04 +0200

    example of code that is not clean

--- testing.js
+++ testing.js
@@ -1,1 +1,26 @@
+(function(){
+'use strict';
+var result;
 
+function getText()  {
+   var getPrompt = prompt("Where do you live?");
+   capitalize(getPrompt);
+   alert(result.join(" "));
+}
+
+function capitalize(text){
+  var splitedText = text.split(" ");
+  var capitalizedText= "";
+  result = [];
+
+  for(var j = 0; j < splitedText.length; j++){
+    capitalizedText = [
+      splitedText[j].charAt(0).toUpperCase(),
+      splitedText[j].substring(1)
+    ].join("");
+    result.push(capitalizedText);
+  }
+}
+
+  document.getElementById('main_button').onclick = getText;
+}());//end wrapper iife
