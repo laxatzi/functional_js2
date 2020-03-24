6660d13b111f30b845c0806de0c28f43973fadcc
laxatzi <laxatzi@yahoo.com>
2020-03-24 13:16:45 +0200

    example of prototypal instantiation in prototype.js file

--- prototypes.js
+++ prototypes.js
@@ -199,3 +199,36 @@ console.log(taoTheBear.name); // 9
 // What if instead of creating a separate object to manage our methods (like we are doing with animalMethods), we just put each of those methods on the Animal function's prototype? Then all we'd have to do is instead of using Object.create to delegate to animalMethods, we could use it to delegate to Animal.prototype. We call this pattern 'Prototypal Instantiation'. 
 
 // Prototypal Instantiation
+
+(function(){
+  'use strict';
+
+  function Animal(name, energy){
+    let animal  = Object.create(Animal.prototype);
+    animal.name = name;
+    animal.energy = energy;
+
+      return animal;  
+  }
+
+  Animal.prototype.eat = function(amount){
+    console.log(`${this.name} is eating`);
+    this.energy += amount;
+  };
+
+  Animal.prototype.sleep =function(length) {
+    console.log(`${this.name} is sleeping.`);
+    this.energy += length;
+  };
+
+  Animal.prototype.play = function(length) {
+    console.log(`${this.name} is playing.`);
+    this.energy -= length;
+  };
+
+  const leo = Animal('leo', 7);
+  const snoopy = Animal('snoopy', 10);
+  leo.eat(10); // "leo is eating"
+  snoopy.play(5); // "snoopy is playing"
+
+}());//end wrapper iife
