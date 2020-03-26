c83130cb466eb9f9e1bd7bdd8d2740dae10eb525
laxatzi <laxatzi@yahoo.com>
2020-03-26 12:15:48 +0200

    achieving the same result with use of the new keyword

--- practise.js
+++ practise.js
@@ -50,26 +50,26 @@ console.log(child.city);  // New York
   'use strict';
 
   function Animal(name, energy){
-   const animal = Object.create(animalMethods);
+   const animal = Object.create(Animal.prototype);
    animal.name = name;
    animal.energy = energy;
 
    return animal;
 }
 
-const animalMethods = {
-   eat(amount){
-      console.log(`${this.name} is eating`);
-      this.energy += amount;
-   },
-   play(hours){
-      console.log(`${this.name} is eating`);
+Animal.prototype.eat = function(amount){
+   console.log(`${this.name} is eating`);
+   this.energy += amount;
+};
+
+Animal.prototype.play = function(hours){
+   console.log(`${this.name} is eating`);
       this.energy -= hours;
-   },
-   sleep(hours){
-      console.log(`${this.name} is sleeping`);
+};
+
+Animal.prototype.sleep = function(hours){
+   console.log(`${this.name} is sleeping`);
       this.energy += hours;
-   }
 };
 
 const tiger = Animal("Sirhan", 10);
@@ -77,7 +77,18 @@ const panda = Animal("Tao-Tao", 7);
 
 console.log(tiger.name);
 console.log(panda.name);
-console.log(panda.sleep);
 
+}());//end wrapper iife
 
+// using new keyword instead of manually delegate
+
+(function(){
+   'use strict';
+   function Animal(name, energy){
+      this.name = name;
+      this.energy = energy;
+   }
+  
+   const tiger =  new Animal("Sirhan", 10);
+   const panda = new Animal("Tao-Tao", 7);
 }());//end wrapper iife
