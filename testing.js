99f7cbad0c6aa81c0d49c73e72b60e38e11a5241
laxatzi <laxatzi@yahoo.com>
2020-03-27 11:49:02 +0200

    comment of code in testing.js file

--- testing.js
+++ testing.js
@@ -64,3 +64,22 @@
  });
 
 }());//end wrapper iife
+
+// Commenting on our OOP code
+/* 
+  Structurally this is a completely different approach to building a program.
+  Object Oriented Code has some advantage as long as some disadvantages.
+
+  The good:
+  -- The methods that we want to use are being defined specific to the type of object that is going to use them which add some convenience. And we have also defined them on the prototype of that object rather than directly on the instances and that allows us to conserve memory.
+  We only need one copy of those methods and every instance can use them.
+
+  -- We have used the new addEventListener instead of onClick and that is more versatile because it allows us to listen for different types of events.
+
+ The not so good:
+ -- We are defining all of our methods to the prototype of this object. We are doing that because they need access to the variables that we are scoped to this inside of the constructor. That is kind of an awkward construct and it is hard to keep track of while you are doing your development. 
+ -- We are still using looping in order to go through all of the array elements. 
+    We know looping can be messy. For one thing, creating a loop like this relies on having an extra variable, such as counter. It is good to avoid creating extra state that you have to keep track of, if possible. And also avoid creating extra variables.
+-- The loop that we are creating is changing a variable outside of itself. And that can make it difficult to reason logically about what the code is doing.
+-- Although this code is easier to test than the imperative code we saw earlier, it is still relatively difficult to test because our object definition is spread across the prototype in multiple places.
+*/
